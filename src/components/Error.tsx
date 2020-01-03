c3feaf1236ef519cf29730d87220ea91f0aafc0c
Rafał Wójcik <rafalwojcik3@gmail.com>
2020-01-03 14:42:24 +0100

    remove unused code from Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -8,20 +8,5 @@ export const Error = () => (
     <div className="hero">
       <h1 className="title">Error!</h1>
     </div>
-
-    {/* <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        text-align: center;
-        font-size: 48px;
-      }
-    `}</style> */}
   </div>
 );
