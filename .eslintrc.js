059e6f10793026d46806acf7da5bdc939620a34c
Rafał Wójcik <rafalwojcik3@gmail.com>
2020-01-04 23:41:15 +0100

    disable conflicting rule with prettier and add arrow parens

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
     '@typescript-eslint/prefer-interface': 'off',
     indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/indent': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/camelcase': 'off',
